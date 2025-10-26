f7c70542218272e39ca134398448291ef76134ff
Cory's Personal <corycates8298@gmail.com>
2025-10-26 04:45:12 +0000

    great....we are in testing....yay great job....oh i found something on t

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 
 export function Header({ title }: HeaderProps) {
   return (
-    <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm sm:h-16 sm:px-6">
+    <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 backdrop-blur-sm sm:h-16 sm:px-6">
       <div className="block md:hidden">
         <SidebarTrigger />
       </div>
